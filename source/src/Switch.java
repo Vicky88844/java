6ac6e9447466f9b20ad03c6cecd76b887070294e
Vicky88844 <vikneshrs555@gmail.com>
2023-12-03 12:07:21 +0530

    switch basics

---
+++ source/src/Switch.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Switch {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String vegetables=in.next();
+
+        switch (vegetables) {
+            case"tomato":
+                System.out.println("tomato is red");
+                break;
+            case "potato":
+                System.out.println("potato is brown");
+                break;
+            case "carrot":
+                System.out.println("carrot is orange");
+                break;
+            case "beetroot":
+                System.out.println("beetroot is purple");
+                break;
+            default:
+                System.out.println("please enter a vegetable");
+        }
+    }
+}
